f292d9c2fb8b5891566eb1bfe05ed7d4d54332b9
Zeza <siraphop.des@gmail.com>
2025-05-06 21:16:57 +0700

    first commit

---
+++ Pipe Thickness Calculation.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.title("Pipe Thickness Calculation")
+
+label = Label(text="HELLO WORLD!",font=30).pack()
+labe2 = Label(text="CAT",font=30).pack()
+
+button_1 = Button(root, text="SAVE").pack()
+
+
+
+root.geometry("800x500")
+root.mainloop()
